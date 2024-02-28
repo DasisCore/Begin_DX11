a804961dafcb7a0da9d4f119219361c6e00829e7
HeoJaeyoung <gj30347896@gmail.com>
2024-02-29 02:41:08 +0900

    GameObject 클래스 생성

--- GameCodeing/Game.cpp
+++ GameCodeing/Game.cpp
@@ -15,41 +15,8 @@ void Game::Init(HWND hwnd)
 	_hWnd = hwnd;
 
 	_graphics = make_shared<Graphics>(hwnd);
-	_vertexBuffer = make_shared<VertexBuffer>(_graphics->GetDevice());
-	_indexBuffer = make_shared<IndexBuffer>(_graphics->GetDevice());
-	_inputLayout = make_shared<InputLayout>(_graphics->GetDevice());
-	_geometry = make_shared<Geometry<VertexTextureData>>();
-	_vertexShader = make_shared<VertexShader>(_graphics->GetDevice());
-	_pixelShader = make_shared<PixelShader>(_graphics->GetDevice());
-	_constantBuffer = make_shared<ConstantBuffer<TransformData>>(_graphics->GetDevice(), _graphics->GetDeviceContext());
-	_texture1 = make_shared<Texture>(_graphics->GetDevice());
-
-	_rasterizerState = make_shared<RasterizerState>(_graphics->GetDevice());
-	_samplerState = make_shared<SamplerState>(_graphics->GetDevice());
-	_blendState = make_shared<BlendState>(_graphics->GetDevice());
-
 	_pipeline = make_shared<Pipeline>(_graphics->GetDeviceContext());
-
-
-	// 삼각형 생성
-		// Vertex Data
-		GeometryHelper::CreateRectangle(_geometry);
-
-		// VertexBuffer
-		_vertexBuffer->Create(_geometry->GetVertices());
-
-		// IndexBuffer
-		_indexBuffer->Create(_geometry->GetIndices());
-
-	// 쉐이더 로드
-	_vertexShader->Create(L"Default.hlsl", "VS", "vs_5_0");
-
-	_inputLayout->Create(VertexTextureData::descs, _vertexShader->GetBlob());	// 구조체의 생김새 자체를 Layout으로 만듬.
-
-	_pixelShader->Create(L"Default.hlsl", "PS", "ps_5_0");
-
-	_texture1->Create(L"Mush.png");
-	_constantBuffer->Create();
+	_gameObject = make_shared<GameObject>(_graphics->GetDevice(), _graphics->GetDeviceContext());
 }
 
 
@@ -57,17 +24,8 @@ void Game::Init(HWND hwnd)
 void Game::Update()
 {
 	// scale rotation translation // SRT
-	
-	Matrix matScale = Matrix::CreateScale(_localScale / 3);
-	Matrix matRotation = Matrix::CreateRotationX(_localRotation.x);
-	matRotation *= Matrix::CreateRotationY(_localRotation.y);
-	matRotation *= Matrix::CreateRotationZ(_localRotation.z);
-	Matrix matTranslation = Matrix::CreateTranslation(_localPosition);
+	_gameObject->Update();
 
-	Matrix matWorld = matScale * matRotation * matTranslation; // SRT
-	_transformData.matWorld = matWorld;
-
-	_constantBuffer->CopyData(_transformData);
 }
  
 void Game::Render()
@@ -76,22 +34,7 @@ void Game::Render()
 
 	// IA - VS - RS - PS - OM
 	{
-		PipelineInfo info;
-		info.inputLayout = _inputLayout;
-		info.vertexShader = _vertexShader;
-		info.pixelShader = _pixelShader;
-		info.rasterizerState = _rasterizerState;
-		info.blendState = _blendState;
-		_pipeline->UpdatePipeline(info);
-
-		auto _deviceContext = _graphics->GetDeviceContext();
-		_pipeline->SetVertexBuffer(_vertexBuffer);
-		_pipeline->SetIndexBuffer(_indexBuffer);
-		_pipeline->SetConstantBuffer(0, SS_VertexShader, _constantBuffer);
-		_pipeline->SetTexture(0, SS_PixelShader, _texture1);
-		_pipeline->SetSamplerState(0, SS_PixelShader, _samplerState);
-
-		_pipeline->DrawIndexed(_geometry->GetIndexCount(), 0, 0);
+		_gameObject->Render(_pipeline);
 	}
 
 	_graphics->RenderEnd();
--- GameCodeing/Game.h
+++ GameCodeing/Game.h
@@ -1,53 +1,22 @@
 #pragma once
 
 #include "Graphics.h"
+#include "GameObject.h"
 
 class Game
 {
 private:
 	HWND _hWnd;
-	// uint32 _width = 0;
-	// uint32 _height = 0;
+
 	shared_ptr<Graphics> _graphics;
 	shared_ptr<Pipeline> _pipeline;
+
+	shared_ptr<GameObject> _gameObject;
 
 public:
 	void Init(HWND _hwnd);
 	void Update();
 	void Render();
-
-private:
-	// Geometry -> 언리얼에서는 메쉬의 정보를 나타냄. 
-	shared_ptr<Geometry<VertexTextureData>> _geometry;
-
-	shared_ptr<VertexBuffer> _vertexBuffer;
-	shared_ptr<IndexBuffer> _indexBuffer;
-	shared_ptr<InputLayout> _inputLayout;
-
-	// VS
-	shared_ptr<VertexShader> _vertexShader;
-
-	// RS
-	shared_ptr<RasterizerState> _rasterizerState;
-
-	// PS
-	shared_ptr<PixelShader> _pixelShader;
-
-	// SRV - shader resource view
-	shared_ptr<Texture> _texture1;
-	
-	shared_ptr<SamplerState> _samplerState;
-	shared_ptr<BlendState> _blendState;
-
-
-private:
-	TransformData _transformData;
-	shared_ptr<ConstantBuffer<TransformData>> _constantBuffer;
-
-	Vec3 _localPosition = { 0.f, 0.f, 0.f };
-	Vec3 _localRotation = { 0.f, 0.f, 0.f };
-	Vec3 _localScale = { 1.f, 1.f, 1.f };
-
 
 public:
 	Game();
