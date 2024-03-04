4e49aacf859fed53d49e327d160b63563ab644b6
HeoJaeyoung <gj30347896@gmail.com>
2024-03-05 03:39:09 +0900

    RenderManager

--- GameCodeing/Game.cpp
+++ GameCodeing/Game.cpp
@@ -6,6 +6,7 @@
 #include "TimeManager.h"
 #include "InputManager.h"
 #include "ResourceManager.h"
+#include "RenderManager.h"
 
 unique_ptr<Game> GGame = make_unique<Game>();
 
@@ -23,7 +24,6 @@ void Game::Init(HWND hwnd)
 	_hWnd = hwnd;
 
 	_graphics = make_shared<Graphics>(hwnd);
-	_pipeline = make_shared<Pipeline>(_graphics->GetDeviceContext());
 
 	_input = make_shared<InputManager>();
 	_input->Init(hwnd);
@@ -37,6 +37,9 @@ void Game::Init(HWND hwnd)
 	_resource = make_shared<ResourceManager>(_graphics->GetDevice());
 	_resource->Init();
 
+	_render = make_shared<RenderManager>(_graphics->GetDevice(), _graphics->GetDeviceContext());
+	_render->Init();
+
 	SCENE->LoadScene(L"Test");
 }
 
@@ -44,16 +47,12 @@ void Game::Init(HWND hwnd)
 
 void Game::Update()
 {
-	_graphics->RenderBegin();
-
 	TIME->Update();
 	INPUT->Update();
 	SCENE->Update();
-
-	_graphics->RenderEnd();
 }
  
 void Game::Render()
 {
-
+	RENDER->Update(_graphics);
 }
